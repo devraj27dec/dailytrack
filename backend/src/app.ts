b7c9c46d20244fbf51edef313b361328402bdb03
Devraj Rajput <rajkirar2002.pc@gmail.com>
2025-10-30 13:20:52 +0530

    update package-json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,8 +4,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 const app = express();
 const PORT = process.env.PORT || 3000;
-// import UserRoute from './routes/user.route.js'
-// import TaskRoute from './routes/task.route.js'
+import UserRoute from './routes/user.route.js'
+import TaskRoute from './routes/task.route.js'
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,8 +15,8 @@ app.get('/', (req:any, res:any) => {
   res.json('Hello, World!');
 });
 
-// app.use('/api/v1/user', UserRoute)
-// app.use('/api/v1/task' , TaskRoute)
+app.use('/api/v1/user', UserRoute)
+app.use('/api/v1/task' , TaskRoute)
 
 
 
